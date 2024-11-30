223ef958cc0f85d2db94fdd575af013d65294f94
morentic <mkopp4191@gmail.com>
2024-11-30 15:17:00 +0100

    define security roles

--- src/Entity/BuildOrder.php
+++ src/Entity/BuildOrder.php
@@ -3,11 +3,21 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity]
 #[ApiResource]
+#[Get(security: "is_granted('PUBLIC_ACCESS')")]
+#[GetCollection(security: "is_granted('PUBLIC_ACCESS') or is_granted('IS_AUTHENTICATED_ANONYMOUSLY')")]
+#[Post(security: "is_granted('ROLE_USER')")]
+#[Put(security: "is_granted('ROLE_ADMIN') or object.owner == user")]
+#[Delete(security: "is_granted('ROLE_ADMIN')")]
 class BuildOrder
 {
     #[ORM\Id]
